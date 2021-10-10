1bf8e073fbd7368025cf6ea3ae820c8fcebc537f
Dudrie <Dudrie@gmx.de>
2021-10-10 15:34:04 +0200

    Add solution for labyrinth exercise.

--- src/main/kotlin/04_Labyrinth.kt
+++ src/main/kotlin/04_Labyrinth.kt
@@ -1,9 +1,44 @@
-import de.github.dudrie.hamster.imperative.de.stoppeSpiel
+import de.github.dudrie.hamster.imperative.de.*
+
+fun istLinksFrei(): Boolean {
+    dreheNachLinks()
+    val istFrei = istVorDirFrei()
+    dreheNachRechts()
+    return istFrei
+}
+
+fun istRechtsFrei(): Boolean {
+    dreheNachRechts()
+    val istFrei = istVorDirFrei()
+    dreheNachLinks()
+    return istFrei
+}
+
+fun dreheUm() {
+    repeat(2) {
+        dreheNachLinks()
+    }
+}
 
 fun main() {
-    starteLabyrinthSpiel(1)
+    starteLabyrinthSpiel(0)
+    setzeSpielGeschwindigkeit(10f)
 
-    // TODO: Implementiere
+    while (!liegtEinKornAufDeinemFeld()) {
+        if (istLinksFrei()) {
+            dreheNachLinks()
+            laufe()
+        } else if (istVorDirFrei()) {
+            laufe()
+        } else if (istRechtsFrei()) {
+            dreheNachRechts()
+            laufe()
+        } else {
+            dreheUm()
+            laufe()
+        }
+    }
 
+    sammleKornAuf()
     stoppeSpiel()
 }
