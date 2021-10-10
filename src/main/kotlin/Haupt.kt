33abca00eace3f85a0045dcbfdb132351fc289f9
Dudrie <Dudrie@gmx.de>
2021-10-10 12:07:14 +0200

    Initial commit

---
+++ src/main/kotlin/Haupt.kt
@@ -0,0 +1,12 @@
+import de.github.dudrie.hamster.imperative.de.starteSpiel
+
+fun main() {
+    // Starte das Spiel (für die Aufgabe)
+    starteSpiel()
+
+    // Implementiere die Lösung
+
+
+    // Stoppe das Spiel
+
+}
